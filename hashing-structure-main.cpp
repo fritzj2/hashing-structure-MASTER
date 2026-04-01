380459db2de78d516692850333f692e8993dadaa
fritzj2 <fritzj2@berea.edu>
2026-04-01 10:29:59 -0400

    Began rewriting literall everything

--- hashing-structure-main.cpp
+++ hashing-structure-main.cpp
@@ -9,7 +9,17 @@ using namespace std;
 
 int main()
 {
-	// Create an empty unordered_map
+	vector<string> words = { "apple", "banana", "orange", "grape", "apple", "kiwi", "melon", "banana", "pear", "peach",
+		"cherry", "apple", "plum", "mango", "grape", "lemon", "lime", "banana", "orange", "fig",
+		"date", "kiwi", "melon", "apple", "papaya", "pear", "grape", "cherry", "banana", "mango" };
+
+	unordered_map<string, int> wordCount;
+	while (!words.empty()){
+
+	
+	return 0;
+	}
+/*	// Create an empty unordered_map
 	unordered_map<char, string> grademap;
 
 	// Insert elements in map
@@ -28,9 +38,11 @@ int main()
 
 	char key = 'N';
 
+		
+
 	// If key not found in map iterator to end is returned 
 	if (grademap.find(key) == grademap.end())
-		cout << key << " not found\n\n";
+		cout << key << " not found\n\n" << endl;
 	// If key found then iterator to that key is returned 
 	else
 		cout << "Found " << key << "\n\n";
@@ -59,7 +71,7 @@ int main()
 		cout << element.first << "\t" << element.second << endl;
 
 	char enter;
-	cin >> enter;
+	cin >> enter;*/
 
 	return 0;
 }
